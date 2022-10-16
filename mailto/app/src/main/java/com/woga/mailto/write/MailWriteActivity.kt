f21645fc8969169cdad62a47c3b1d4f6d7998e81
hyowan <woga1999@gmail.com>
2022-10-17 01:12:34 +0900

    email 양식 맞지 않으면 textfield view 색 변경

--- mailto/app/src/main/java/com/woga/mailto/write/MailWriteActivity.kt
+++ mailto/app/src/main/java/com/woga/mailto/write/MailWriteActivity.kt
@@ -2,7 +2,6 @@ package com.woga.mailto.write
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,11 +12,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
-import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.TextFieldColors
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,24 +29,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.toColor
 import com.woga.mailto.R
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MailWriteActivity : AppCompatActivity() {
 
 
-    val viewModel: MailWriteViewModel by viewModels()
+//    val viewModel: MailWriteViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            mailWriteView()
+            MailWriteView()
         }
     }
 
     @Composable
-    fun mailWriteView() {
+    fun MailWriteView(viewModel: MailWriteViewModel = viewModel()) {
+
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -59,9 +60,15 @@ class MailWriteActivity : AppCompatActivity() {
             OutlinedTextField(
                 value = emailText,
                 modifier = Modifier.fillMaxWidth(),
-                onValueChange = { emailText = it },
+                onValueChange = {
+                    viewModel.checkValidEmail(it)
+                    emailText = it
+                },
                 maxLines = 1,
-                label = { Text("본인의 이메일을 적어주세요") }
+                label = {
+                    EmailTextViewLabel(viewModel.isEmailPattern.value ?: true)
+                },
+                colors = getTextFieldColors(viewModel.isEmailPattern.value ?: true)
             )
 
             Spacer(modifier = Modifier.padding(top = 10.dp, start = 20.dp, end = 20.dp))
@@ -72,40 +79,66 @@ class MailWriteActivity : AppCompatActivity() {
                 modifier = Modifier.fillMaxWidth(),
                 onValueChange = { title = it },
                 maxLines = 1,
-                label = { Text("제목") }
+                label = { Text("제목") },
+                colors = getTextFieldColors(true)
             )
 
             Spacer(modifier = Modifier.padding(top = 10.dp, start = 20.dp, end = 20.dp))
-            var description by remember { mutableStateOf("") }
+            var content by remember { mutableStateOf("") }
 
             OutlinedTextField(
-                value = description,
+                value = content,
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(300.dp),
-                onValueChange = { description = it },
-                label = { Text("내용") }
+                onValueChange = { content = it },
+                label = { Text("내용") },
+                colors = getTextFieldColors(true)
             )
             Spacer(modifier = Modifier.padding(bottom = 10.dp))
 
             Spacer(modifier = Modifier.padding(top = 10.dp, start = 20.dp, end = 20.dp))
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Button(
+                    shape = RoundedCornerShape(34.dp),
                     modifier = Modifier.width(100.dp),
-                    onClick = { ActivityCompat.finishAfterTransition(this@MailWriteActivity)},
-                    colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(R.color.gray_60), contentColor = colorResource(R.color.gray_400))
+                    onClick = { ActivityCompat.finishAfterTransition(this@MailWriteActivity) },
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = colorResource(R.color.gray_60),
+                        contentColor = colorResource(R.color.gray_400)
+                    )
                 ) {
                     Text("취소")
                 }
                 Spacer(modifier = Modifier.padding(start = 20.dp, end = 20.dp))
                 Button(
+                    shape = RoundedCornerShape(34.dp),
                     modifier = Modifier.width(100.dp),
-                    onClick = { /*TODO*/ }
+                    onClick = { viewModel.sendEmail(emailText, title, content)}
                 ) {
                     Text("전송")
                 }
             }
         }
+    }
+
+    @Composable
+    fun EmailTextViewLabel(isValidEmail: Boolean) {
+        return if (isValidEmail) {
+            Text("본인의 이메일을 적어주세요")
+        } else {
+            Text("이메일 양식이 올바르지 않습니다.")
+        }
+    }
+
+    @Composable
+    fun getTextFieldColors(isValidEmail: Boolean): TextFieldColors {
+        return TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = if (isValidEmail) colorResource(R.color.green_600) else colorResource(R.color.red_900),
+            unfocusedBorderColor = colorResource(R.color.gray_600),
+            focusedLabelColor = if (isValidEmail) colorResource(R.color.green_600) else colorResource(R.color.red_900),
+            cursorColor = colorResource(R.color.gray_600)
+        )
     }
 
 }
--- mailto/app/src/main/java/com/woga/mailto/write/MailWriteViewModel.kt
+++ mailto/app/src/main/java/com/woga/mailto/write/MailWriteViewModel.kt
@@ -1,7 +1,25 @@
 package com.woga.mailto.write
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class MailWriteViewModel : ViewModel() {
 
+    private val _isEmailPattern = MutableLiveData<Boolean>()
+    val isEmailPattern: LiveData<Boolean>
+        get() = _isEmailPattern
+
+
+    fun checkValidEmail(email: String) {
+        val pattern: Pattern = Pattern.compile("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}\$")
+        val matcher: Matcher = pattern.matcher(email)
+        _isEmailPattern.value = matcher.matches()
+    }
+
+    fun sendEmail(email: String, title: String, content: String) {
+
+    }
 }
